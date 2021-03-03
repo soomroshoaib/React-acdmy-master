b70c426e7ef5b85217efe44d35c0e888b7e94dff
soomroshoaib <soomroshoaib178@gmail.com>
2021-03-03 07:03:56 -0800

    Updates

---
+++ precache-manifest.dc64db9b23abd2f2c94f2d12998fbc9d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2117b12c58c42bd72d1f852a30e2c938",
+    "url": "/React-acdmy-master/index.html"
+  },
+  {
+    "revision": "9d7d07f7d4b7e2aa8b3d",
+    "url": "/React-acdmy-master/static/css/main.02157e7f.chunk.css"
+  },
+  {
+    "revision": "e71ba0eff58d36a3f654",
+    "url": "/React-acdmy-master/static/js/2.cf56782a.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/React-acdmy-master/static/js/2.cf56782a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d7d07f7d4b7e2aa8b3d",
+    "url": "/React-acdmy-master/static/js/main.79e74f8d.chunk.js"
+  },
+  {
+    "revision": "b94cc61ed651adc84ca9",
+    "url": "/React-acdmy-master/static/js/runtime-main.1249308f.js"
+  },
+  {
+    "revision": "662725ae7c07ede404a197e33b90a5d4",
+    "url": "/React-acdmy-master/static/media/cake.662725ae.png"
+  },
+  {
+    "revision": "cd02f446c826659f56908a6636ea22f1",
+    "url": "/React-acdmy-master/static/media/course-2.cd02f446.jpg"
+  },
+  {
+    "revision": "1246863a78d66ed04d0f7e28bb4a0856",
+    "url": "/React-acdmy-master/static/media/icomoon.1246863a.eot"
+  },
+  {
+    "revision": "1980b8e0feb9a4e871cb8d7ca5fc929f",
+    "url": "/React-acdmy-master/static/media/icomoon.1980b8e0.woff"
+  },
+  {
+    "revision": "3b310fb1fc7b60b06defc45090738b3d",
+    "url": "/React-acdmy-master/static/media/icomoon.3b310fb1.svg"
+  },
+  {
+    "revision": "43d9f7083f7d21ddc9be022d80abd84e",
+    "url": "/React-acdmy-master/static/media/icomoon.43d9f708.ttf"
+  },
+  {
+    "revision": "575dac4d81c80fe9d04d165fc7dad27f",
+    "url": "/React-acdmy-master/static/media/logo-coral.575dac4d.svg"
+  }
+]);
